f14a7a11cebb7461297e0f34df68b4eb28032f87
scelsic2 <scelsic2@gmail.com>
2023-05-24 11:51:45 -0400

    base react app with connections

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import axios from 'axios'
+import './App.css';
+import Header from './components/Header';
+
+function App(props) {
+   return (
+    <>
+    
+        <Header />
+
+    </>
+    ) 
+}
+
+
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const path = require('path');
+const session = require("express-session");
+const PORT = process.env.PORT || 3001;
+const db = require("./config/connection");
+
+const api_routes = require("./routes/api_routes");
+const auth_routes = require("./routes/auth_routes");
+
+const app = express();
+app.use(express.json());
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: process.env.PORT ? true : false },
+  })
+);
+
+app.use("/api", api_routes);
+app.use("/auth", auth_routes);
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+db.once("open", () => {
+  app.listen(PORT, () => console.log("server started on port %s", PORT));
+});
