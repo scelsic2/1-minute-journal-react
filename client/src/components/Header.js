86d2e2f983b80b0c133cf36fa4fdf0e013dbc800
scelsic2 <scelsic2@gmail.com>
2023-05-25 11:46:50 -0400

    mobile nav animates

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,41 @@
 import React, {useState} from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import logo from '../assets/images/package.png'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FiBookOpen } from 'react-icons/fi';
+import { BsPersonCircle } from 'react-icons/bs'
 
 function Header(props) {
+    
+    const [isOpen, setIsOpen] = useState(false)
+    
+    const toggleMenu = () => {
+        setIsOpen((open) => !open)
+    }
+
     return (
 
         <>
             <header>
 
-            <img src={logo} height="30px" className="logo"/>
-            <h3 className="title">1-Minute Journal</h3>
+            <div className='left'>
+                <img src={logo} height="30px" className="logo"/>
+                <h3 className="title">1-Minute Journal</h3>
+            </div>
+
+            <div className='nav-div'>
+
+                <nav className='mobile' onClick={toggleMenu}>
+                    <GiHamburgerMenu color="var(--accent)" fontSize={25}/>
+                </nav>
+
+                <nav className={`right ${isOpen ? "is-open" : ""}`}>
+                    <a href='/entires'><FiBookOpen color="var(--accent)" fontSize={23}/></a>
+                    <a href='/settings'><BsPersonCircle color="var(--accent)" fontSize={20}/></a>
+                </nav>
+
+            </div>
+
 
             </header>
         </>
--- client/src/components/Prompt.js
+++ client/src/components/Prompt.js
@@ -4,6 +4,7 @@ import prompts from "../assets/js/prompts";
 
 function Prompt(props) {
   const [randomPrompt, setRandomPrompt] = useState("");
+  const [showPrompt, setShowPrompt] = useState(true);
 
   // Random Prompt Generator
   useEffect(() => {
@@ -13,24 +14,47 @@ function Prompt(props) {
   const generateRandomPrompt = () => {
     const newRandomPrompt = prompts[Math.floor(Math.random() * prompts.length)];
     setRandomPrompt(newRandomPrompt);
+    setShowPrompt(true);
   };
 
   const handleNewPrompt = () => {
     generateRandomPrompt();
   };
 
+  const handleClearPrompt = () => {
+    setShowPrompt(false);
+  };
+
   return (
     <>
-      <div className="prompt-container">
-        <div className="prompt">{randomPrompt}</div>
-      </div>
+      {showPrompt && (
+        <div className="prompt-container">
+          <div className="prompt">{randomPrompt}</div>
+        </div>
+      )}
 
       <div className="footer-buttons">
         <button className="new-prompt prompt-button" onClick={handleNewPrompt}>
           New Prompt
         </button>
-        <button className="clear-prompt prompt-button">Clear Prompt</button>
+
+        <button
+          className="clear-prompt prompt-button"
+          onClick={handleClearPrompt}
+        >
+          Clear Prompt
+        </button>
       </div>
+
+      <form className="new-entry">
+        <textarea
+          rows="8"
+          cols="50"
+          className="textarea"
+          placeholder="If you only had 1 minute to journal about your day, what would you record?"
+        ></textarea>
+        <button className="save">Save</button>
+      </form>
     </>
   );
 }
